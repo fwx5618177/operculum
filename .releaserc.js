cc9a5b1525e2bdaf3520becd1215a8622aa5caa9
fwx5618177 <fengwenxuan2006@126.com>
2023-09-28 00:54:14 +0800

    feat: update

--- .releaserc.js
+++ .releaserc.js
@@ -1,10 +1,21 @@
 module.exports = {
     branches: ['master'],
     plugins: [
-        '@semantic-release/commit-analyzer',
+        '@semantic-release/commit-analyzer', // 解析 commit 信息
         '@semantic-release/release-notes-generator',
-        '@semantic-release/changelog',
+        [
+            '@semantic-release/changelog',
+            {
+                changelogFile: 'CHANGELOG.md', // 把发布日志写入该文件
+            },
+        ],
         // "@semantic-release/npm",
         // "@semantic-release/github",
+        [
+            '@semantic-release/git',
+            {
+                assets: ['CHANGELOG.md', 'package.json'], // 前面说到日志记录和版本号是新增修改的，需要 push 回 Git
+            },
+        ],
     ],
 }
--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # CLI
 
 A CLI for parsing and handling resume templates.
+
+## dev
+
+```bash
+feat：    新功能（feature）
+fix：     修补bug
+docs：    更新了文档，或者更新了注释（documentation）
+style：   代码格式调整，比如执行了 format、更改了 Tab 显示等（不影响代码运行的变动），非指CSS的样式
+refactor：重构代码（即不是新增功能，也不是修改bug的代码变动）
+perf：    对项目或者模块进行了性能优化
+test：    增加单元测试或者其他测试相关
+chore：   构建过程或辅助工具的变动
+build：   影响编译的一些更改，比如更改了maven插件、增加了npm 的过程等
+ci：      持续集成方面的更改
+```
